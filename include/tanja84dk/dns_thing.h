0f6d6844630b55c4cd593727439f89236539eb9c
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2025-02-08 09:50:15 +0000

    moved gen_ip_network_name

--- include/tanja84dk/dns_thing.h
+++ include/tanja84dk/dns_thing.h
@@ -9,26 +9,7 @@
 
 namespace Tanja84dk {
 
-std::string gen_ip_network_name(const std::string& ip_as_0_range) noexcept {
-    std::string buffer = "";
-    std::string return_data = "";
-    int sep_counter = 0;
-    bool break_out = false;
-    for (int i = 0; i < ip_as_0_range.length(); i++) {
-        if (sep_counter == 3) {
-            break;
-        } else {
-            buffer = ip_as_0_range[i];
-        }
-        if (buffer == ".") {
-            sep_counter += 1;
-            return_data.append("-");
-        } else {
-            return_data.append(buffer);
-        }
-    }
-    return return_data;
-}
+std::string gen_ip_network_name(const std::string& ip_as_0_range) noexcept;
 
 std::string gen_rev(uint8_t& digit, Tanja84dk::IP_Address& IP_Obj, const std::string& domain) noexcept {
     int digit_length = std::to_string(digit).length();
--- src/dns_thing.cpp
+++ src/dns_thing.cpp
@@ -1,1 +1,24 @@
 #include <tanja84dk/dns_thing.h>
+
+#include <string>
+
+std::string Tanja84dk::gen_ip_network_name(const std::string& ip_as_0_range) noexcept {
+    std::string buffer = "";
+    std::string return_data = "";
+    int sep_counter = 0;
+    bool break_out = false;
+    for (int i = 0; i < ip_as_0_range.length(); i++) {
+        if (sep_counter == 3) {
+            break;
+        } else {
+            buffer = ip_as_0_range[i];
+        }
+        if (buffer == ".") {
+            sep_counter += 1;
+            return_data.append("-");
+        } else {
+            return_data.append(buffer);
+        }
+    }
+    return return_data;
+}
