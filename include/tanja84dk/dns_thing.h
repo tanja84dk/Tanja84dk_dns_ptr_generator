ff54240e1284e4a22f1aa033814b4ad52e84a7b7
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2025-02-08 09:55:32 +0000

    moved gen_rev

--- include/tanja84dk/dns_thing.h
+++ include/tanja84dk/dns_thing.h
@@ -3,33 +3,13 @@
 
 #include <tanja84dk/classes.h>
 
-#include <fstream>
-#include <iostream>
 #include <string>
 
 namespace Tanja84dk {
 
 std::string gen_ip_network_name(const std::string& ip_as_0_range) noexcept;
 
-std::string gen_rev(uint8_t& digit, Tanja84dk::IP_Address& IP_Obj, const std::string& domain) noexcept {
-    int digit_length = std::to_string(digit).length();
-    int padding = 8 - digit_length;
-    std::string ip_network_str =
-        fmt::format("{0}-{1}-{2}", IP_Obj.get_first(), IP_Obj.get_second(), IP_Obj.get_third());
-
-    if (IP_Obj.host_list.count(digit)) {
-        if (IP_Obj.host_list.find(digit)->second.back() != '.') {
-            return fmt::format("{0}{1}IN      PTR     {2}.{3}.", digit, std::string(padding, ' '),
-                               IP_Obj.host_list.find(digit)->second, domain);
-
-        } else {
-            return fmt::format("{0}{1}IN      PTR     {2}", digit, std::string(padding, ' '),
-                               IP_Obj.host_list.find(digit)->second);
-        }
-    }
-    return fmt::format("{0}{1}IN      PTR     {2}-{0}.dyn.{3}.", digit, std::string(padding, ' '), ip_network_str,
-                       domain);
-}
+std::string gen_rev(uint8_t& digit, Tanja84dk::IP_Address& IP_Obj, const std::string& domain) noexcept;
 
 std::map<int, std::string> read_input_file(const std::string& filename) noexcept {
     std::map<int, std::string> output;
--- src/dns_thing.cpp
+++ src/dns_thing.cpp
@@ -1,5 +1,9 @@
-#include <tanja84dk/dns_thing.h>
+#include "tanja84dk/dns_thing.h"
 
+#include <fmt/core.h>
+#include <tanja84dk/classes.h>
+
+#include <map>
 #include <string>
 
 std::string Tanja84dk::gen_ip_network_name(const std::string& ip_as_0_range) noexcept {
@@ -21,4 +25,24 @@ std::string Tanja84dk::gen_ip_network_name(const std::string& ip_as_0_range) noe
         }
     }
     return return_data;
+}
+
+std::string gen_rev(uint8_t& digit, Tanja84dk::IP_Address& IP_Obj, const std::string& domain) noexcept {
+    int digit_length = std::to_string(digit).length();
+    int padding = 8 - digit_length;
+    std::string ip_network_str =
+        fmt::format("{0}-{1}-{2}", IP_Obj.get_first(), IP_Obj.get_second(), IP_Obj.get_third());
+
+    if (IP_Obj.host_list.count(digit)) {
+        if (IP_Obj.host_list.find(digit)->second.back() != '.') {
+            return fmt::format("{0}{1}IN      PTR     {2}.{3}.", digit, std::string(padding, ' '),
+                               IP_Obj.host_list.find(digit)->second, domain);
+
+        } else {
+            return fmt::format("{0}{1}IN      PTR     {2}", digit, std::string(padding, ' '),
+                               IP_Obj.host_list.find(digit)->second);
+        }
+    }
+    return fmt::format("{0}{1}IN      PTR     {2}-{0}.dyn.{3}.", digit, std::string(padding, ' '), ip_network_str,
+                       domain);
 }
