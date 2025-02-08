57a39a95560d3c403c3e53269e9d61fb99dd4e55
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2025-02-08 10:13:40 +0000

    splitted up save_file

--- include/tanja84dk/tools.h
+++ include/tanja84dk/tools.h
@@ -2,25 +2,13 @@
 #define TANJA84DK_DNSGEN_TOOLS_H
 #include <tanja84dk/classes.h>
 
-#include <algorithm>
-#include <ctime>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <sstream>
 #include <string>
 #include <vector>
 
 namespace Tanja84dk {
 bool file_exist(const std::string& filename) noexcept;
 
-void save_file(const std::string& filename, const std::vector<std::string>& vector_data) noexcept {
-    std::ofstream fh(filename);
-    std::ostream_iterator<std::string> output_iterator(fh, "\n");
-    std::copy(std::begin(vector_data), std::end(vector_data), output_iterator);
-    fh.close();
-}
+void save_file(const std::string& filename, const std::vector<std::string>& vector_data) noexcept;
 
 int get_date_stamp() noexcept {
     time_t timestamp;
--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,19 @@
 #include "tanja84dk/tools.h"
 
+#include <algorithm>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <string>
+#include <vector>
 
 bool Tanja84dk::file_exist(const std::string& filename) noexcept { return std::filesystem::exists(filename); }
+
+void Tanja84dk::save_file(const std::string& filename, const std::vector<std::string>& vector_data) noexcept {
+    std::ofstream fh(filename);
+    std::ostream_iterator<std::string> output_iterator(fh, "\n");
+    std::copy(std::begin(vector_data), std::end(vector_data), output_iterator);
+    fh.close();
+}
